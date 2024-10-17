1dc50f34ce8845357ed4bd7f6cea05ec52cacc8c
Qingsong Hu <kingsanhu@gmail.com>
2024-10-17 20:59:32 +0800

    Finish implementing animation

--- HW1/animation.py
+++ HW1/animation.py
@@ -1,46 +1,52 @@
+import math
+
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 from HW1.utils import generate_rhombus, generate_trace
 
-WINDOW_WIDTH = 80
-WINDOW_HEIGHT = 60
+WINDOW_WIDTH = 200
+WINDOW_HEIGHT = 150
 MARGIN = 15
 RADIUS = 6
 
+rec_start = (MARGIN, MARGIN)
+rec_width = float(WINDOW_WIDTH - 2 * MARGIN)
+rec_height = float(WINDOW_HEIGHT - 2 * MARGIN)
+edge_rhombus = generate_rhombus((rec_start[0], rec_start[1] +
+                                 rec_height / 2.0), (rec_start[0] +
+                                                     rec_width / 2.0, rec_start[1] + rec_height))
 
-def main():
-    rec_start = (MARGIN, MARGIN)
-    rec_width = float(WINDOW_WIDTH - 2 * MARGIN)
-    rec_height = float(WINDOW_HEIGHT - 2 * MARGIN)
-    rhombus_vertices = generate_rhombus((rec_start[0], rec_start[1] +
-                                        rec_height / 2.0), (rec_start[0] +
-                                        rec_width / 2.0, rec_start[1] + rec_height))
-    fig, ax = plt.subplots()
-    ax.set_aspect('equal')
-    ax.set_xlim(0, WINDOW_WIDTH)
-    ax.set_ylim(0, WINDOW_HEIGHT)
-    ax.add_patch(mpatches.Rectangle(rec_start, rec_width, rec_height, fc='none',
-                                    edgecolor='blue',
-                                    linewidth=1, linestyle='--'))
-    ax.add_patch(mpatches.Polygon(rhombus_vertices, closed=True,
-                                  edgecolor='blue', fc='none'))
-    plt.show()
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+ax.set_xlim(0, WINDOW_WIDTH)
+ax.set_ylim(0, WINDOW_HEIGHT)
+ax.add_patch(mpatches.Rectangle(rec_start, rec_width, rec_height, fc='none',
+                                edgecolor='blue',
+                                linewidth=1, linestyle='--'))
+ax.add_patch(mpatches.Polygon(edge_rhombus, closed=True,
+                              edgecolor='blue', fc='none'))
+theta = math.atan((edge_rhombus[1][1] - edge_rhombus[0][1]) / (edge_rhombus[1][0] - edge_rhombus[0][0]))
+trace_rhombus = generate_rhombus((edge_rhombus[0][0] + RADIUS/math.sin(theta), edge_rhombus[0][1]),
+                                 (edge_rhombus[1][0], edge_rhombus[1][1] - RADIUS/math.cos(theta)))
+trace_x, trace_y = generate_trace(trace_rhombus)
+ball = ax.add_patch(mpatches.Circle((trace_x[0], trace_y[0]), RADIUS,
+                                    color='red',
+                                    fc='none'))
 
 
-def generate_rhombus(point1, point2):
+def update(frame):
     """
-    Generate all four vertices according to the given points.
-    :param point1: coordinates of the left most point
-    :param point2: coordinates of the highest point
-    :return: list of all four vertices
+    For updating each frame of the animation
+    :param frame: represent which frame
+    :return: updated Artiest objects
     """
-    point3 = (2 * point2[0] - point1[0], point1[1])
-    point4 = (point2[0], 2 * point1[1] - point2[1])
-    return [point1, point2, point3, point4]
+    ball.set_center((trace_x[frame % trace_x.size],
+                     trace_y[frame % trace_y.size]))
 
 
 
-if __name__ == '__main__':
-    main()
+ani = animation.FuncAnimation(fig, update, interval=1, repeat=True)
+plt.show()
+
