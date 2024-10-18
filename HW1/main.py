45008fe227886e89d7739a3cd38210529a167e3a
Qingsong Hu <kingsanhu@gmail.com>
2024-10-18 10:55:38 +0800

    Finish user input for problem 1

--- HW1/main.py
+++ HW1/main.py
@@ -1,14 +1,49 @@
-from HW1.ui import get_input
+import math
+
+import matplotlib.pyplot as plt
+from numpy.ma.core import minimum
+
+from HW1.scanning_algorithms import bresenham_line_standard, bresenham_circle
+from HW1.ui import Drawer, get_input_line, get_input_circle
 
 
 def main():
+    drawer = Drawer()
     print("This is the demo of my HW1, do you want to check problem 1 or problem 2?")
     problem = input("Please input 1 for problem 1 or 2 for problem 2: ")
     if problem == '1':
         print("You choose problem 1...")
-        start, end = get_input()
+        line_or_circle = input("Do you want to draw a line or a circle? "
+                               "Enter 1 for line, 2 for circle: ")
+        subdivision = input("If you want to set subdivision to see the "
+                            "effect of line scanning, please input "
+                            "the number of subdivisions(default is 1000). "
+                            "If you don't, just press the ENTER: ")
+        if line_or_circle == '1':
+            start, end = get_input_line()
+            if subdivision:
+                x, y = bresenham_line_standard(start, end, int(subdivision))
+            else:
+                x, y = bresenham_line_standard(start, end)
+            xlim_left = min(start[0], end[0]) - 5
+            xlim_right = max(start[0], end[0]) + 5
+            ylim_down = min(start[1], end[1]) - 5
+            ylim_up = max(start[1], end[1]) + 5
+            drawer.set_fig((xlim_left, xlim_right), (ylim_down, ylim_up))
+            drawer.line_drawer(x, y)
+        elif line_or_circle == '2':
+            print("You choose to draw a circle...")
+            center, radius = get_input_circle()
+            if subdivision:
+                x, y = bresenham_circle(center, int(radius), int(subdivision))
+            else:
+                x, y = bresenham_circle(center, int(radius))
+            drawer.set_fig((center[0] - int(radius) - 5, center[0] + int(radius) + 5),
+                           (center[1] - int(radius) - 5, center[1] + int(radius) + 5))
+            drawer.circle_drawer(x, y, center, int(radius))
+        plt.show()
     elif problem == '2':
-        print("You choose problem 2, please run the test.py in the tests folder")
+        print("You choose problem 2, run the animation")
     else:
         print("Invalid input, please try again.")
         main()
