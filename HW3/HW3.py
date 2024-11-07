d2391b44210ea5a868b58d3405b0379041628626
Qingsong Hu <kingsanhu@gmail.com>
2024-11-07 15:58:52 +0800

    Fix bug
    Mandelbrot should plot the grid points after iterations, the color maps
    to the counts within 2

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,18 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-z0 = 1 + 1j
-c = 1j
-iter_count = 800
-mandelbrot_set = np.zeros(iter_count)
-mandelbrot_set[0] = z0
+max_iter = 800
+grid_size = 1000
+xlim = [-2.0, 1.0]
+ylim = [-1.5, 1.5]
 
-iterFunc = lambda x: x * x + c
+x = np.linspace(xlim[0], xlim[1], grid_size)
+y = np.linspace(ylim[0], ylim[1], grid_size)
+grid_x, grid_y = np.meshgrid(x, y)
+mandelbrot_set = grid_x + 1j * grid_y
+c = np.copy(mandelbrot_set)
+iter_count = np.ones(mandelbrot_set.shape)
 
-for i in range(1, iter_count):
-    mandelbrot_set[i] = iterFunc(mandelbrot_set[i - 1])
+iterFunc = lambda z: z * z + c
 
+for i in range(0, max_iter):
+    mandelbrot_set = iterFunc(mandelbrot_set)
+    iter_count += np.abs(mandelbrot_set) <= 2
+
+color = np.log(iter_count)
+
+plt.figure(figsize=(10, 10))
 fig, ax = plt.subplots()
-ax.plot(mandelbrot_set.real, mandelbrot_set.imag)
+ax.scatter(grid_x, grid_y, c=color, cmap='viridis')
 
 plt.show()
