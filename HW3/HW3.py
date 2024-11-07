9104dd5346847af05875f842b2d6c5986553a0ba
Qingsong Hu <kingsanhu@gmail.com>
2024-11-07 16:31:28 +0800

    Optimize code

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -3,17 +3,16 @@ import matplotlib.pyplot as plt
 
 max_iter = 800
 grid_size = 1000
-xlim = [-2.0, 1.0]
-ylim = [-1.5, 1.5]
+xrange = [-2.0, 1.0]
+yrange = [-1.5, 1.5]
+iterFunc = lambda z: z * z + c
 
-x = np.linspace(xlim[0], xlim[1], grid_size)
-y = np.linspace(ylim[0], ylim[1], grid_size)
+x = np.linspace(xrange[0], xrange[1], grid_size)
+y = np.linspace(yrange[0], yrange[1], grid_size)
 grid_x, grid_y = np.meshgrid(x, y)
 mandelbrot_set = grid_x + 1j * grid_y
 c = np.copy(mandelbrot_set)
 iter_count = np.ones(mandelbrot_set.shape)
-
-iterFunc = lambda z: z * z + c
 
 for i in range(0, max_iter):
     mandelbrot_set = iterFunc(mandelbrot_set)
@@ -21,8 +20,7 @@ for i in range(0, max_iter):
 
 color = np.log(iter_count)
 
-plt.figure(figsize=(10, 10))
-fig, ax = plt.subplots()
-ax.scatter(grid_x, grid_y, c=color, cmap='viridis')
+plt.figure(figsize=(9, 9))
+plt.scatter(grid_x, grid_y, c=color, cmap='viridis')
 
 plt.show()
