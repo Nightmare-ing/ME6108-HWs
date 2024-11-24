ede710ff27fee9e309212e9f9c105769faf746cc
Qingsong Hu <kingsanhu@gmail.com>
2024-11-24 22:58:43 +0800

    Modify the data structure
    Think it's better to make the time as the first dimension, so we have a
    list of matrices at each times stamps, and don't have to access the
    matrices by passing the time dimension

--- HW4/bezier_curve.py
+++ HW4/bezier_curve.py
@@ -7,9 +7,9 @@ class BezierCurve:
         Initialize a BezierCurve class, draw the curve with the Casteljau
         algorithm, store all the intermediate computed points to
         `computed_points`, which is a numpy array.
-        Construct the np.array in the form (i, j, t, cor), `i` stands for
-        the layer index, `j` stands for the point index, `t` stands for the
-        coordinates at the specific time stamp, `cor` is a 2d array, with x
+        Construct the np.array in the form (t, i, j, cor), `t` stands for the
+        coordinates at the specific time stamp, `i` stands for the layer
+        index, `j` stands for the point index, `cor` is a 2d array, with x
         and y coordinates
         :param control_points: the control points to construct the curve,
         with the form of np.array in two dimension, each row is a pair of
@@ -18,8 +18,8 @@ class BezierCurve:
         """
         self.n = control_points.shape[1]
         self.time_stamps = np.linspace(0, 1, time_splits, endpoint=True)
-        self.computed_points = np.zeros((self.n, self.n, time_splits, 2))
-        self.computed_points[0] = np.tile(control_points,
+        self.computed_points = np.zeros((time_splits, self.n, self.n, 2))
+        self.computed_points[:, 0] = np.tile(control_points,
                                           (time_splits, 1, 1))
 
     def generate(self, i, j):
@@ -28,10 +28,10 @@ class BezierCurve:
         :param i: layer `i` in recursion
         :param j: `j`th point
         """
-        self.computed_points[i, j] = (((1 - self.time_stamps) *
-                                      self.computed_points[i - 1,
-                                      j]) - self.time_stamps *
-                                      self.computed_points[i - 1, j + 1])
+        self.computed_points[:, i, j] = (((1 - self.time_stamps) *
+                                      self.computed_points[:, i - 1, j])
+                                         - self.time_stamps *
+                                      self.computed_points[:, i - 1, j + 1])
 
     def compute(self):
         """
@@ -46,8 +46,8 @@ class BezierCurve:
         Return the control points Artists
         :return: the control points artists for Matplotlib to draw
         """
-        drawing_points = [Circle(self.computed_points[0, index, 0, :].tolist(), 0.1)
-                          for index in range(self.n)]
+        drawing_points = [Circle(item.tolist(), 0.1) for item in
+                                 self.computed_points[0, 0]]
         return drawing_points
 
     
