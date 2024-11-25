876ee017fa76d8ab1b7bfca92c5889c106291728
Qingsong Hu <kingsanhu@gmail.com>
2024-11-25 18:58:10 +0800

    Set limits for axes

--- HW4/main.py
+++ HW4/main.py
@@ -1,5 +1,8 @@
+import os.path
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import numpy as np
 
 from HW4.bezier_curve import BezierCurve
 from utils import read_data_prompt, read_coor
@@ -10,12 +13,12 @@ def main():
     coors = read_coor(file_path, 2)
 
     fig, ax = plt.subplots()
-    ax.set_xlim(-0.5, 1.5)
-    ax.set_ylim(-0.5, 1.5)
-    curve = BezierCurve(ax, coors)
-    # for item in curve.initialize_artists():
-    #     ax.add_line(item)
-    ani = animation.FuncAnimation(fig, curve.update, frames=curve.time_splits, init_func=curve.initialize_artists, interval=100)
+    ax.set_xlim(np.min(coors[:, 0]) - 0.5, np.max(coors[:, 0]) + 0.5)
+    ax.set_ylim(np.min(coors[:, 1]) - 0.5, np.max(coors[:, 1]) + 0.5)
+    curve = BezierCurve(ax, coors, 1000)
+    ani = animation.FuncAnimation(fig, curve.update,
+                                  frames=curve.time_splits,
+                                  init_func=curve.initialize_artists, interval=10, blit=True)
     plt.show()
 
 
